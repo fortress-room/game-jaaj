46a77ca8facf8a65ee437f09c2c9c24c3dfd7ddc
Lucas Coelho <lucascoelhodacosta@gmail.com>
2018-07-21 21:55:25 -0300

    [WIP] handling 8 directions animations

--- Scripts/Character/PlayerMove.gd
+++ Scripts/Character/PlayerMove.gd
@@ -15,7 +15,7 @@ enum {
 }
 var state = IDLE
 
-var animation = 'default'
+var current_animation = 'default'
 var old_animation = 'default'
 
 func _physics_process(delta):
@@ -24,6 +24,7 @@ func _physics_process(delta):
 
 func _move(delta):
 	
+	# Movement controlling handlers
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -speed
 	elif Input.is_action_pressed("ui_right"):
@@ -39,3 +40,24 @@ func _move(delta):
 		motion.y = 0
 	
 	motion = move_and_slide(motion, ground)
+	
+	# Animation control handlers
+	if Input.is_action_pressed("ui_up"):
+		_play_animation()
+		if Input.is_action_pressed("ui_left"):
+			_play_animation()
+		elif Input.is_action_pressed("ui_right"):
+			print('bilu')
+	
+	if Input.is_action_pressed("ui_down"):
+		_play_animation()
+		if Input.is_action_pressed("ui_left"):
+			_play_animation()
+		elif Input.is_action_pressed("ui_right"):
+			print('bolu')
+	
+	
+func _play_animation():
+	if current_animation != old_animation:
+		# TODO: play new animation
+		old_animation = current_animation
