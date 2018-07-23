d990fbd16335c379f8afdea2d2a2229dc4fae40e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2018-07-23 15:34:13 -0300

    Created house lot manager

---
+++ Scripts/Controllers/LotManager.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var house_types
+
+"""
+Notes to do this script:
+	1: This code are going to manage buildings since they need to be created 'til they're finished.
+	2: I need to save into a JSON object every house_type and every resource needed to create to quickly get here.
+	3.1: I need to have a son ui node showing and able to select the materials and send to him to complete the house
+	3.2: And after the 3.1, have to destroy this son (if the house is completed!!!)
+	
+"""
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
