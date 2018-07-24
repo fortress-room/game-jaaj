0d218cee3b95a12b7f5c89d301c38c427b92880e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2018-07-24 15:02:08 -0300

    House lots json file prepared and get random house based on difficult

--- Scripts/Controllers/LotManager.gd
+++ Scripts/Controllers/LotManager.gd
@@ -1,22 +1,30 @@
 extends Node2D
 
-var house_types
+var houses_file_path = "res://Resources/houses.json"
+
+var level = "01"
+var house_type
 
 """
 Notes to do this script:
 	1: This code are going to manage buildings since they need to be created 'til they're finished.
-	2: I need to save into a JSON object every house_type and every resource needed to create to quickly get here.
+	X: I need to save into a JSON object every house_type and every resource needed to create to quickly get here.
 	3.1: I need to have a son ui node showing and able to select the materials and send to him to complete the house
 	3.2: And after the 3.1, have to destroy this son (if the house is completed!!!)
 	
 """
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+	randomize()
+	
+	# Create and open the File
+	var file = File.new()
+	file.open(houses_file_path, file.READ)
+	
+	# Get file as text and parse it into a JSON dictionary
+	var json = file.get_as_text()
+	json = parse_json(json)
+	
+	# Generate a random_index based in current level and assign it to current house
+	var index = randi() % (json["level_" + level].size())
+	house_type = json["level_" + level][index]
